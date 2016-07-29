89f33321f00a7dccddb63d37701dc7d25730ccd6
Pavle Neskovic <pavlenesko@gmail.com>
2016-07-29 12:15:25 +0200

    petnaesthiljadamilijarditikomit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Idemoo nis");
         System.out.println("Idemo niiis" +
-                "");
+                "idemo sniiis niiis");
     }
 }
